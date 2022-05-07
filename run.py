ca80861d98f81dc2f90458e13b208d987e4ea796
Mateus Andrade <38530760+joandesonandrade@users.noreply.github.com>
2022-05-07 20:49:16 -0300

    fck

--- run.py
+++ run.py
@@ -46,7 +46,6 @@ async def addToQueue(request: Request, token: str = ""):
     if "delay" in data:
         delay = float(data["delay"])
 
-
     worker = getWorker.Worker()
     q = Queue(connection=worker.getConnection())
     q.enqueue_in(
