ed3a02e0fabbfec4bf6e5833d70d88dd2033a4f3
Mateus Andrade <38530760+joandesonandrade@users.noreply.github.com>
2022-05-07 20:21:23 -0300

    www

--- Sender.py
+++ Sender.py
@@ -14,7 +14,7 @@ class Request:
             if "redirect" in self.data:
                 response = requests.post(
                     url=url,
-                    json={"idiot": 1}
+                    json=self.data
                 )
 
                 try:
