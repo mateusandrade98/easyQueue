856f58dab3170e298e9d216fa1e2f435df89953f
Mateus Andrade <38530760+joandesonandrade@users.noreply.github.com>
2022-05-07 21:20:39 -0300

    new2000

--- Sender.py
+++ Sender.py
@@ -8,44 +8,7 @@ class Request:
     async def run(self, data: dict = None):
         self.data = data
         if self.data:
-            url = self.data["url"]
 
-            if "redirect" in self.data:
-                requests.post(
-                    url=url,
-                    json=self.data
-                )
-
-                return {"success": 1}
-
-            try:
-                payload = self.data["payload"]
-            except KeyError:
-                payload = {}
-
-            try:
-                headers = self.data["headers"]
-                values_headers = headers.items()
-                headers = {str(key): str(value) for key, value in values_headers}  # convert key:value to string
-            except KeyError:
-                headers = {}
-
-            try:
-                method = self.data["method"]
-            except KeyError:
-                method = "GET"
-
-            try:
-                params = self.data["params"]
-            except KeyError:
-                params = {}
-
-            requests.request(
-                method=method,
-                url=url,
-                headers=headers,
-                data=payload,
-                params=params
-            )
+            # modificar na library worker
 
             return {"success": 1}
